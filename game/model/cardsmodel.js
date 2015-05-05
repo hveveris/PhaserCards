f7c120b8324fcb0b8cf291c9e45586d75f8746cd
Harijs Veveris <harijs.veveris@gmail.com>
2015-05-05 20:25:50 +0200

    Removing excessive arguments

--- game/model/cardsmodel.js
+++ game/model/cardsmodel.js
@@ -1,5 +1,5 @@
 
-CardsModel = function (game, x, y) {
+CardsModel = function () {
 
     var symbols = ["clubs", "hearts", "spades", "diamonds"];
     var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
